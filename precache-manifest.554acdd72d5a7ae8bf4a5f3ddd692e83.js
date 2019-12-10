8e0952b233dad12c6d70e907f3cadd1a171d8641
Koallla <zmiyov2422@gmail.com>
2019-12-10 20:08:28 +0200

    Updates

---
+++ precache-manifest.554acdd72d5a7ae8bf4a5f3ddd692e83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c249caae0742ec93c239187133f28ac",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "ebde8ae002c2a593a507",
+    "url": "/goit-react-hw-02-reader/static/css/main.e9e923a1.chunk.css"
+  },
+  {
+    "revision": "049b53a9120833902814",
+    "url": "/goit-react-hw-02-reader/static/js/2.f35f11d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-reader/static/js/2.f35f11d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebde8ae002c2a593a507",
+    "url": "/goit-react-hw-02-reader/static/js/main.b60e101a.chunk.js"
+  },
+  {
+    "revision": "60bcd26ea4b278ac156d",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.eb2bdb34.js"
+  }
+]);
