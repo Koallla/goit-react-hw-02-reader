566ee4421dcc141bdeb8a78a351e788abfe6de0a
Koallla <zmiyov2422@gmail.com>
2019-12-22 12:08:37 +0200

    delete div

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import Reader from '../Reader/Reader';
 import publications from '../Reader/publications.json';
 
-const App = () => (
-  <div>
-    <Reader items={publications} />
-  </div>
-);
+const App = () => <Reader items={publications} />;
 
 export default App;
